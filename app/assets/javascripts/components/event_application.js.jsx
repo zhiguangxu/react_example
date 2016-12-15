7dd01ae8e1a90e3d09743fc2d37e4e1428f4d1ea
Zhiguang Xu <xuzhiguang@gmail.com>
2016-12-15 18:11:12 -0500

    New/Create event done. Finished with part 1 of the tutorial.

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -22,6 +22,11 @@ var EventApplication = React.createClass({
 	handleSearch: function(events){
 		this.setState({ events: events});
 	},
+	handleAdd: function(event){
+		var events = this.state.events;
+		events.push(event);
+		this.setState({ events: events});
+	},
 	render: function(){
 		return (
 			<div className="container">
@@ -33,7 +38,11 @@ var EventApplication = React.createClass({
 					<div className="col-md-4">
 						<SearchForm handleSearch={this.handleSearch}/>
 					</div>
-				</div>				
+
+					<div className="col-md-8">
+						<NewForm handleAdd={this.handleAdd}/>
+					</div>
+				</div>			
 				<div className="row">
 					<div className="col-md-12">
 						<EventTable events={this.state.events}/>
--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -9,6 +9,19 @@ module Api
     						 "%#{query}%", "%#{query}%", "%#{query}%")
     	render json: events
     end
+    def create
+    	event = Event.new(event_params)
+    	if event.save
+    		render json: event
+    	else
+    		render nothing: true, status: :bad_request
+    	end
+    end
 
+    private
+
+    def event_params
+    	params.require(:event).permit(:name,:description,:event_date,:place)
+    end
   end
 end
