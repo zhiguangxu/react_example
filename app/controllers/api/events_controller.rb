0f2d49645fee17e089ba34b5ebe4cff4ea4e507c
Zhiguang Xu <xuzhiguang@gmail.com>
2016-12-15 21:52:50 -0500

    Done with update.

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class EventsController < ApplicationController
-    before_action :set_event, only: [:destroy]
+    before_action :set_event, only: [:update, :destroy]
     def index
       render json: Event.all
     end
@@ -18,7 +18,13 @@ module Api
     		render nothing: true, status: :bad_request
     	end
     end
-
+    def update
+        if @event.update(event_params)
+            render json: @event
+        else
+            render nothing: true, status: :unprocessable_entity
+        end
+    end
     def destroy
         @event.destroy
         head :no_content
